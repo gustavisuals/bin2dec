a48235c1de6e06228982ac9912bb91bdc4abba25
gustavisual <gustafx.suporte@gmail.com>
2020-04-27 13:15:35 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.png';
+import adImg from './adImg.png';
 import './App.css';
 
-function App() {
+function App() { 
+  const [binaryInput, setBinaryInput] = useState('')
+  const [decimalInput, setDecimalInput] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+    
+  const onButtonSubmit = event => { // Realiza a conversão no envio do formulário.
+
+    event.preventDefault() // Impede a atualização do navegador.    
+    
+    if (binaryInput.match(/^[0-1]+$/g) === null) { // Verifica se possui 0 ou 1.
+      setErrorMessage('Digite um valor binario sem espaços')
+      return
+    }
+
+  setErrorMessage(' ')
+
+  const result = parseInt(binaryInput, 2)
+
+  setDecimalInput(result)
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="content">
+        <br/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br/>
+        <section>
+          <form onSubmit={onButtonSubmit}>
+            {errorMessage && <a> {errorMessage} </a>}
+            <br/>
+            <input
+              type="text"
+              name="binario"
+              maxLength="8"
+              placeholder="Digite um valor binário"
+              value={binaryInput}
+              onChange={event => setBinaryInput(event.target.value)}
+            />
+            <button
+              className="button"
+              type="submit"
+            >
+              convert
+            </button>
+            <input
+              type="text"
+              name="decimal"
+              placeholder="Resultado em decimal"
+              value={decimalInput}
+              disabled
+            />
+          </form>            
+        </section>
+        <br/>        
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          Learn React
+        Clique para seu cupom de desconto!
         </a>
-      </header>
+        <div className="adArea">
+          <img src={adImg} alt=""/>
+        </div>
+        <br/>
+        <p>Gustavo Gomes ® All rights reserved. - 2020</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
